43ea56f42c283785c022d6d21d086145f9e12975
Rui Fernando Hayashi <rfhayashi@gmail.com>
2016-12-27 20:34:14 -0200

    refactoring api and use of test check

--- src/blog_clj/core.clj
+++ src/blog_clj/core.clj
@@ -1,10 +1,19 @@
-(ns blog-clj.core)
+(ns blog-clj.core
+  (:import (java.util UUID)))
 
 (defn create-database []
   {})
 
 (defn update-post [db post]
   (assoc db (:id post) post))
+
+(defn new-id []
+  (.toString (UUID/randomUUID)))
+
+(defn create-post [db]
+  (let [post {:id (new-id)}]
+    {:db-after (update-post db post)
+     :post post}))
 
 (defn exists? [db post]
   (contains? db (:id post)))
--- test/blog_clj/core_test.clj
+++ test/blog_clj/core_test.clj
@@ -1,10 +1,23 @@
 (ns blog-clj.core-test
   (:require [clojure.test :refer :all]
-            [blog-clj.core :refer :all]))
+            [blog-clj.core :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
-(deftest new-post-is-added-to-db
-  (let [db (create-database)
-        post {:id 1}]
-    (is (not (exists? db post)))
-    (is (exists? (update-post db post) post))))
+(defspec new-post-is-added-to-db
+         100
+         (let [db (create-database)]
+           (prop/for-all []
+             (let [{:keys [post db-after]} (create-post db)]
+               (is (not (exists? db post)))
+               (is (exists? db-after post))))))
 
+(defspec post-ids-are-unique
+         100
+         (let [ids* (atom #{})
+               db (create-database)]
+           (prop/for-all []
+                         (let [{:keys [post]} (create-post db)]
+                           (is (not (@ids* (:id post))))
+                           (swap! ids* conj (:id post))))))
